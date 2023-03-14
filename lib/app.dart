08096af152256c9c7248307f76cb725e770198fe
KIHUN KIM <kihunkim@KIHUNui-MacBookAir.local>
2023-03-14 15:53:36 +0900

    init commit

---
+++ lib/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web_view/home.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Web Views',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          fontFamily: 'Arial',
+          textTheme: const TextTheme(
+            labelLarge: TextStyle(color: Colors.white, fontSize: 18.0),
+          )),
+      home: const Home(),
+    );
+  }
+}
